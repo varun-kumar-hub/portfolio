baa734de5f02b818f388bbcaa9cc298694952b90
VarunKumar988 <cvarunkumar455@gmail.com>
2026-07-26 14:09:08 +0530

    Remove dead components, obsolete demo files, unused data, template SVG assets, and unused dependencies

--- src/components/container-text-flip-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { ContainerTextFlip } from "@/components/ui/container-text-flip";
-
-export default function ContainerTextFlipDemo() {
-  return (
-    <ContainerTextFlip
-      words={["better", "modern", "Tyler Durden", "awesome"]}
-    />
-  );
-}
